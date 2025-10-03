949b76d9e050060291118584c6988a6841ca2faf
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-03 13:08:11 +0700

    feat: catpuccin black background

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,10 +2,17 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    config = true,
-
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = '#000000',
+          mantle = '#080808',
+          crust = '#080808',
+        },
+      },
+    },
     init = function()
-      --vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
